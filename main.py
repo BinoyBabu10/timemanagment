c92bf6782eeb87c20e7b7d97a3ff4a4ea3a74aac
Your Name <you@example.com>
2023-10-31 18:49:32 +0530

    Countdown mechanism

--- main.py
+++ main.py
@@ -14,7 +14,10 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
-
+def count_down(count):
+    if count>=0:
+        canvas.itemconfig(timer_text,text=count)
+        window.after(1000,count_down,count-1)
 # ---------------------------- UI SETUP ------------------------------- #
 window=Tk()
 window.title('Pomodore')
@@ -27,9 +30,9 @@ title_label.grid(column=1,row=0)
 canvas=Canvas(width=200,height=224,bg=YELLOW,highlightthickness=0)
 tomato_image=PhotoImage(file="tomato.png")
 canvas.create_image(100,112,image=tomato_image)
-canvas.create_text(100,130,text="00:00",fill="white",font=(FONT_NAME,35,'bold'))
+timer_text=canvas.create_text(100,130,text="00:00",fill="white",font=(FONT_NAME,35,'bold'))
 canvas.grid(column=1,row=1)
-
+count_down(5)
 # Button
 start_button=Button(text="Start")
 start_button.grid(column=0,row=2)
